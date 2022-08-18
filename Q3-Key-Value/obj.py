4a2cd3dba5995c3300cfde662d54371bb222369c
Shubham3521 <shubham3521@gmail.com>
2022-08-19 03:13:28 +0530

    Add files via upload

---
+++ Q3-Key-Value/obj.py
@@ -0,0 +1,14 @@
+
+
+a_dict = {"x":{"y":{"z":"a"}}}
+keys = "y"
+ 
+def iterate(dictionary): 
+    for key, value in dictionary.items(): 
+        if isinstance(value, dict): 
+            iterate(value) 
+            continue
+    if key==keys:
+        print(value)
+ 
+iterate(a_dict)
